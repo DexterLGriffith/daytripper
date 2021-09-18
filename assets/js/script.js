fc3c0cd73d778fc2f20c6dffd67e4403c801ff2c
Allen Cheslik <allencheslik0@gmail.com>
2021-09-18 11:46:45 -0500

    updated fecth API and JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
 // THIS IS THE FUNCTION TO MAKE THE SIDEBAR COLLAPSABLE
-(function($){
-    $(function(){
-  
-      $('.sidenav').sidenav();
-  
-    }); // end of document ready
-  })(jQuery); // end of jQuery name space
+(function ($) {
+  $(function () {
+
+    $('.sidenav').sidenav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
 
 // get sidenav buttons in header to lead to correct places //
 
@@ -41,6 +41,8 @@
 // TODO: Create the function that grabs the google maps data
 function initMap() {
   console.log("Google Maps has been initialized");
+  var googleMapkey = "AIzaSyDmtQ1hzQJFdnivcj0RLybUddmhldyarz8";
+  var mapqueryURL = "https://maps.googleapis.com/maps/api/js?key=AIzaSyDmtQ1hzQJFdnivcj0RLybUddmhldyarz8&callback=initMap"
   // 1. Define an API Query URL
   // 2. Do a fetch call to retrieve the data
   // 3. Make a promise (then)
@@ -48,18 +50,39 @@ function initMap() {
 }
 
 // TODO: Create the function that grabs the openweathermap
+var inputValue = document.querySelector('.city-search');
+var cityNameEl = document.querySelector('.city-name');
+var descriptionEl = document.querySelector('.description');
+var temperatureEl = document.querySelector('.temperature');
+
+
 function initWeather() {
   console.log("initWeather has been initialized");
+
   // 1. Define an API Query URL
   // 2. Do a fetch call to retrieve the data
-  // 3. Make a promise (then)
-  // 4. Error handling 
+  fetch('http://api.openweathermap.org/data/2.5/weather?q=' + inputValue.value + '&units=imperial&524901&appid=95d25656c865f0b0125b6ad3ef11eab0')
+    .then(Response => Response.json())
+    .then(data => {
+      var cityName = data['name'];
+      var tempValue = data['main']['temp']
+      var descriptionValue = data['weather'][0]['description'];
+
+      cityNameEl.innerHTML = cityName
+      temperatureEl.innerHTML = tempValue;
+      descriptionEl.innerHTML = descriptionValue;
+
+    })
+    // 3. Make a promise (then)
+    // 4. Error handling 
+    .catch(err => {
+      console.error(err);
+    })
 }
 
 // TODO: Add an eventlistener to the search button that fires both functions
 $("#search-btn").on("click", function () {
+  var searchTerm = $("#input").val
   initMap();
   initWeather();
 });
-
-
