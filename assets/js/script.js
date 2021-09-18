73211d0522529a540e2914c7ad4c3eba442ffdee
Candra Fried <candracodes@gmail.com>
2021-09-18 10:30:34 -0500

    Properlt referenced the javascript file in index.html, and created two functions to play around with for fetch and response.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// THIS IS THE FUNCTION TO MAKE THE SIDEBAR COLLAPSABLE
 (function($){
     $(function(){
   
@@ -18,18 +19,10 @@
 
 
 
-
-
-
 // function to get openweatherAPI working for select zipcode //
 
 
-
-
-
 // add eventlistener attached to search bar to make search bar fetch for the zipcode? //
-
-
 
 // function of what you should pack //
 
@@ -38,22 +31,35 @@
 // if loop for different weather conditions (rain, sun, snow, etc.) for select zipcode //
 
 
-
-
-
-
-
 // function of different places to go see using the googlemaps or openstreepmaps apis for given zipcode //
-
-
-
 
 
 // yelp review api with review stars for places //
 
 
 
+// TODO: Create the function that grabs the google maps data
+function initMap() {
+  console.log("Google Maps has been initialized");
+  // 1. Define an API Query URL
+  // 2. Do a fetch call to retrieve the data
+  // 3. Make a promise (then)
+  // 4. Error handling 
+}
 
+// TODO: Create the function that grabs the openweathermap
+function initWeather() {
+  console.log("initWeather has been initialized");
+  // 1. Define an API Query URL
+  // 2. Do a fetch call to retrieve the data
+  // 3. Make a promise (then)
+  // 4. Error handling 
+}
 
+// TODO: Add an eventlistener to the search button that fires both functions
+$("#search-btn").on("click", function () {
+  initMap();
+  initWeather();
+});
 
 
