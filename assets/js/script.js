2389b5fe35bf63b6f427c878e98fb904751f2c2b
Dexter Griffith <dextergriffith@sbcglobal.net>
2021-09-17 22:37:16 -0500

    added basic javascript comments for potential functions needed on website

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,4 +5,55 @@
   
     }); // end of document ready
   })(jQuery); // end of jQuery name space
-  
+
+// get sidenav buttons in header to lead to correct places //
+
+
+
+// funcation variables //
+
+
+
+//  start of moment function to get today's current date //
+
+
+
+
+
+
+// function to get openweatherAPI working for select zipcode //
+
+
+
+
+
+// add eventlistener attached to search bar to make search bar fetch for the zipcode? //
+
+
+
+// function of what you should pack //
+
+
+
+// if loop for different weather conditions (rain, sun, snow, etc.) for select zipcode //
+
+
+
+
+
+
+
+// function of different places to go see using the googlemaps or openstreepmaps apis for given zipcode //
+
+
+
+
+
+// yelp review api with review stars for places //
+
+
+
+
+
+
+
